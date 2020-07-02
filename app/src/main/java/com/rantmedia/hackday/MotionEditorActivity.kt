67a725c817ddf2fc8a39fc6b56584c9cd599e89b
Admin <scott@rantmedia.com>
2020-07-02 16:06:22 +0100

    3 animations

--- app/src/main/java/com/rantmedia/hackday/MotionEditorActivity.kt
+++ app/src/main/java/com/rantmedia/hackday/MotionEditorActivity.kt
@@ -2,6 +2,9 @@ package com.rantmedia.hackday
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_motion_editor.*
+
 
 class MotionEditorActivity : Activity() {
 
@@ -9,5 +12,21 @@ class MotionEditorActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_editor)
 
+        val motionContainer =  findViewById<MotionLayout>(R.id.motion_layout)
+
+        blue_animation_btn.setOnClickListener {
+            motionContainer.setTransition(R.id.blue_transition)
+            motionContainer.transitionToEnd()
+        }
+
+        orange_animation_btn.setOnClickListener {
+            motionContainer.setTransition(R.id.orange_transition)
+            motionContainer.transitionToEnd()
+        }
+
+        curve_btn.setOnClickListener {
+            motionContainer.setTransition(R.id.curve_transition)
+            motionContainer.transitionToEnd()
+        }
     }
 }
