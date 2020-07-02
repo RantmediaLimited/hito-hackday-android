a560bf3b843b339565922b367c880d58bdf3c94c
Admin <scott@rantmedia.com>
2020-07-02 10:19:08 +0100

    MotionEditorActivity.kt created with layout

---
+++ app/src/main/java/com/rantmedia/hackday/MotionEditorActivity.kt
@@ -0,0 +1,13 @@
+package com.rantmedia.hackday
+
+import android.app.Activity
+import android.os.Bundle
+
+class MotionEditorActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_editor)
+
+    }
+}
