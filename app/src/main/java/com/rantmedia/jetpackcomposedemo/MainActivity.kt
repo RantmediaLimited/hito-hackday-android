b8c28d0d1929aa10e811ab37b8308757bc9d4702
shaban <shaban@rantmedia.com>
2021-06-18 16:24:48 +0100

    MainActivity example implemented.

--- app/src/main/java/com/rantmedia/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/rantmedia/jetpackcomposedemo/MainActivity.kt
@@ -3,36 +3,99 @@ package com.rantmedia.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rantmedia.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MainComposable {
+                ScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainComposable(content: @Composable () -> Unit) {
+    JetpackComposeDemoTheme {
+        // Define a Surface to set a different background color.
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeDemoTheme {
-        Greeting("Android")
+    MainComposable {
+        ScreenContent()
+    }
+}
+
+@Composable
+fun ScreenContent() {
+    Column {
+        NewsStory(header = "Jetpack Compose", body1 = "Cardiff", body2 = "June 2021")
+        NewsStory(
+            header = "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+            body1 = "Davenport, California",
+            body2 = "December 2018"
+        )
+        Counter()
+    }
+}
+
+@Composable
+fun NewsStory(header: String, body1: String, body2: String) {
+    val typography = MaterialTheme.typography
+    Column(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.header_image),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = header,
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(body1, style = typography.body2)
+        Text(body2, style = typography.body2)
+    }
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+    Button(
+        onClick = { count.value++ },
+        modifier = Modifier.padding(start = 16.dp)
+    ) {
+        Text("I've been clicked ${count.value} times")
     }
 }
